1e8937a771392252100a3b90d37b4bbe2b8f84cf
Piyush <piyush.jha@hyperverge.co>
2022-05-14 16:11:58 +0530

    Deployment Phase

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');//package
-const path=require('path');
+const path = require('path');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
@@ -28,7 +28,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/ads', adRoutes);
 app.use('/api/chat', chatRoutes)
 
-// __dirname = path.resolve();
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
 
@@ -36,7 +36,8 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     );
 } else {
-    app.get("/", (req, res) => {x
+    app.get("/", (req, res) => {
+        x
         res.send("API is running..");
     });
 }
